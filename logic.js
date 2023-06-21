3ed644b46dbed11ec303141d98c8c35fa1dda9f5
marcilustra <theknifebox69@gmail.com>
2023-06-21 13:48:48 +0300

    Eventlistner Disable = button if empty display

--- logic.js
+++ logic.js
@@ -1,4 +1,19 @@
+// Get references to the input and button elements
+const input = document.getElementById('display');
+const button = document.getElementById('button');
+
+// Add an event listener to the input element
+input.addEventListener('input', function() {
+  // Toggle the disabled attribute based on the input value
+  if (input.value.trim() !== '') {
+    button.disabled = false;  // Enable the button
+  } else {
+    button.disabled = true;   // Disable the button
+  }
+});
+
 function clearField () {
     let input = document.getElementById("display");
     input.value = '';
+    button.disabled = true;
 }
