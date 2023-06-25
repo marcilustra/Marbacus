3acf6c4102dd79b819076b64c77267b213ecd4fa
marcilustra <theknifebox69@gmail.com>
2023-06-25 17:15:41 +0300

    Created Delete & Decimal functions

--- Mark II/script.js
+++ Mark II/script.js
@@ -1,3 +1,6 @@
+var answer = 0;
+var left = 0;
+var right = 0;
 
 function clearLabelText() {
     document.getElementById("top").textContent = "";
@@ -10,5 +13,23 @@ function appendNumber (val) {
         document.getElementById("bottom").textContent = val;    
     } else {
         document.getElementById("bottom").textContent += val;
+    }
+}
+
+function dotIT (val) {
+    var labelText = document.getElementById("bottom").textContent;
+
+    if(!labelText.includes(".")) {
+        document.getElementById("bottom").textContent += val;
+    }
+}
+
+function delEat () {
+    var labelText = document.getElementById("bottom").textContent;
+
+    if(labelText.length == 1) {
+        document.getElementById("bottom").textContent = 0;
+    } else {
+        document.getElementById("bottom").textContent = labelText.substring(0, labelText.length -1)
     }
 }
