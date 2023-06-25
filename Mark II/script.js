7698fe798a4e72fdc1ab7f6ebecab569df5c4720
marcilustra <theknifebox69@gmail.com>
2023-06-26 02:24:57 +0300

    Allow the equal sign to work with + operand.

--- Mark II/script.js
+++ Mark II/script.js
@@ -1,17 +1,43 @@
+/*These are global variables
+answer holds the result of an operation
+left hods the first side value that is input
+right handles the second side of an operation value
+
+e.g 98 + 34 = 132
+left = 98
+right = 34
+answer = 132
+*/
 var answer = 0;
 var left = 0;
 var right = 0;
 
-function clearLabelText() {
+//Clears the display and the three variables as well.
+function clearDisplay() {
     document.getElementById("top").textContent = "";
     document.getElementById("bottom").textContent = "0";
+}
+function clearAll() {
+    clearDisplay();
     left = "";
     right = "";
     answer = "";
 }
 
+function delEat () {
+    var labelText = document.getElementById("bottom").textContent;
+
+    if(labelText.length == 1) {
+        clearDisplay();
+    } else {
+        document.getElementById("bottom").textContent = labelText.substring(0, labelText.length -1)
+    }
+}
+
+//Adds the number value of the button to the bottom display string
 function appendNumber (val) {
     var labelText = document.getElementById("bottom").textContent;
+    //Check if the value is the default zero to replace it with a different number
     if(labelText === "0") {
         document.getElementById("bottom").textContent = val;
     } else {
@@ -19,26 +45,16 @@ function appendNumber (val) {
     }
 }
 
+//Places a single decimal point on a number.
 function dotIT (val) {
     var labelText = document.getElementById("bottom").textContent;
-
+//Check if string already includes a dot character
     if(!labelText.includes(".")) {
         document.getElementById("bottom").textContent += val;
     }
 }
 
-function delEat () {
-    var labelText = document.getElementById("bottom").textContent;
-
-    if(labelText.length == 1) {
-        document.getElementById("bottom").textContent = 0;
-    } else {
-        document.getElementById("bottom").textContent = labelText.substring(0, labelText.length -1)
-    }
-}
-
 function plus(val) {
-    var bottomText = document.getElementById("bottom").textContent;
     left += parseFloat(document.getElementById("bottom").textContent);
     if (left) {
         document.getElementById("bottom").textContent = "0";
@@ -47,4 +63,16 @@ function plus(val) {
     answer = parseFloat(left) + parseFloat(right);
     document.getElementById("top").textContent = answer + " " + val;
     console.log(answer);
+}
+
+function displayFinal () {
+    console.log("Answer should be output here.");
+    let finaloperand = document.getElementById("top").textContent.slice(-1);
+    switch(finaloperand) {
+        case '+':
+            answer = parseFloat(document.getElementById("bottom").textContent) + parseFloat(answer);
+            break;
+    }
+    document.getElementById("bottom").textContent = answer;
+    document.getElementById("top").textContent = "";
 }
