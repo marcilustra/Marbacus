714332710c223b63f28657e8f5626cdf0984e6dc
Marci Lustra <theknifebox69@gmail.com>
2023-07-18 14:12:45 +0300

    Delete button Functionality

--- HTML,CSS,JS/Mark III/script.js
+++ HTML,CSS,JS/Mark III/script.js
@@ -1,3 +1,16 @@
+//deleteCut removes the last item in the bottom content
+//one by one until it is down to zero.
+function deleteCut() {
+    var del = document.getElementById("bottomContent").innerHTML;
+    if (del.length > 1) {
+        document.getElementById("bottomContent").innerHTML = del.substring(0, del.length - 1);
+    } else {
+        document.getElementById("bottomContent").innerHTML = "0";
+    }
+}
+
+//Clear bottom takes out the bottom content and replaces it with 0
+//in one click
 function clearBottom() {
     document.getElementById("bottomContent").innerHTML = "0";
 }
